f95f7de829c980b13af0b92781ab4101197f5654
Mohammed alJaser <aljaserm7@gmail.com>
2019-02-16 10:25:17 -0500

    Custom Renderer IOS, Android,

---
+++ ExpensesApp/ExpensesApp.Android/CustomRenders/ProgressBarCR.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+//using Android.Widget;
+using ExpensesApp.Droid.CustomRenders;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ProgressBar), typeof(ProgressBarCR))]
+namespace ExpensesApp.Droid.CustomRenders 
+{
+    class ProgressBarCR : ProgressBarRenderer
+    {
+        public ProgressBarCR(Context context) : base (context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.ProgressBar> e)
+        {
+            base.OnElementChanged(e);
+            if (double.IsNaN(e.NewElement.Progress))
+                Control.ProgressDrawable.SetTint( Color.FromHex("#00B9AE").ToAndroid());
+            else if (e.NewElement.Progress < 0.3)
+                Control.ProgressDrawable.SetTint(Color.FromHex("#008DD5").ToAndroid());
+            else if (e.NewElement.Progress < 0.5)
+                Control.ProgressDrawable.SetTint(Color.FromHex("#2D76BA").ToAndroid());
+            else if (e.NewElement.Progress < 0.7)
+                Control.ProgressDrawable.SetTint(Color.FromHex("#5A5F9F").ToAndroid());
+            else if (e.NewElement.Progress < 0.9)
+                Control.ProgressDrawable.SetTint(Color.FromHex("#B3316A").ToAndroid());
+            else
+                Control.ProgressDrawable.SetTint(Color.FromHex("#e01a4f").ToAndroid());
+
+            Control.ScaleY = 4.0f;
+        }
+    }
+}
---
+++ ExpensesApp/ExpensesApp.iOS/CustomRenders/TextCellCR.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpensesApp.iOS.CustomRenders;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(TextCell), typeof(TextCellCR))]
+namespace ExpensesApp.iOS.CustomRenders
+{
+    class TextCellCR : TextCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell= base.GetCell(item, reusableCell, tv);
+            switch (item.StyleId)
+            {
+                case "none":
+                    cell.Accessory = UITableViewCellAccessory.None;
+                    break;
+                case "checkmark":
+                    cell.Accessory = UITableViewCellAccessory.Checkmark;
+                    break;
+                case "detail-button":
+                    cell.Accessory = UITableViewCellAccessory.DetailButton;
+                    break;
+                case "detail-disclosure-button":
+                    cell.Accessory = UITableViewCellAccessory.DetailDisclosureButton;
+                    break;
+                case "disclosure":
+                default:
+                    cell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+                    break;
+
+
+            }
+            return cell;
+        }
+    }
+}
